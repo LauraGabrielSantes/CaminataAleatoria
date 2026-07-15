a0fc6e1c018493ba86a3ac7d3bdd44bf0f9708c4
Alejandro <banali1604@gmail.com>
2026-07-15 16:52:11 -0600

    mensaje m hehco

--- 1/caminataAleatoria.py
+++ 1/caminataAleatoria.py
@@ -4,84 +4,84 @@ from event import Event
 from model import Model
 
 class CaminataAleatoria(Model):
-   contadorMensajes=0
-   encontrado = False
-   def init(self):
-     print ("\nInicio funciones", self.id)
-     print ("Mis vecinos son:", self.neighbors)
-     self.visitado=False
-     self.padre = self.id
-     self.disponibles = self.neighbors
-     self.mis_recursos = self.llenaRecurso()
-     self.id
-     print ("[",self.id,"]: Mis recursos son: ", self.mis_recursos,"\n")
+    contadorMensajes=0
+    encontrado = False
+    def init(self):
+        print ("\nInicio funciones", self.id)
+        print ("Mis vecinos son:", self.neighbors)
+        self.visited=False
+        self.father = self.id
+        self.avalibleNodes = self.neighbors
+        self.myResources = self.llenaRecurso()
+        self.idFromResourcesFound = []
+        print ("[",self.id,"]: Mis recursos son: ", self.mis_recursos,"\n")
 
-   # Aqui se definen las acciones concretas que deben ejecutarse cuando se
-   # recibe un evento
-   def receive(self, event):
-    origen=event.getSource()
-    evento=event.getName()
-    if isinstance (evento,tuple):
-      nombre=evento[0]
-      listaMensaje = evento [1]
-      recursoBuscado = listaMensaje [0]
-      emisorBusqueda = listaMensaje [1]
-      longitud_recibido = listaMensaje [2]
-    else:
-      nombre = evento
+    def randomSend( self , avalibleNodes , idRoot , walkingLength , resourcesToFound ) :
 
-    if nombre == "INICIA":
-     self.visitado=True
-     emisorBusqueda = self.id
-     self.nodos_con_recurso = []
-     self.TTL = TTL_recibido
-     print ("\n[", self.id, "]: recibí INICIA y soy el nodo emisor de búsqueda con TTL ", self.TTL," en t=",self.clock," \n")
-     self.TTL -= 1
-     mensaje = ("M", [recursoBuscado,emisorBusqueda,self.TTL])
-     for vecino in self.neighbors:
-       newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
-       CaminataAleatoria.contadorMensajes += 1
-       self.transmit(newevent)
-    elif nombre == "M":
-      print ("[", self.id, "]: Recibí M de ", origen," con TTL = ", TTL_recibido," en t=",self.clock," ")
-      if not self.visitado:
-        self.visitado=True
-        self.TTL = TTL_recibido
-        print ("[", self.id, "]: actualizo TTL a ", self.TTL," \n")
-        if self.localizaRecurso(self.mis_recursos, recursoBuscado):
-          newevent = Event("ENCONTRADO", self.clock + 1.0, emisorBusqueda, self.id)
-          CaminataAleatoria.contadorMensajes += 1
-          self.transmit(newevent)
-        if self.TTL > 0:
-         self.TTL -= 1
-         mensaje = ("M",[recursoBuscado,emisorBusqueda,self.TTL])
-         for vecino in self.neighbors:
-          if vecino != origen:  
-            newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
-            CaminataAleatoria.contadorMensajes += 1
-            self.transmit(newevent)
-      else:
-        if self.TTL < TTL_recibido:
-          self.TTL = TTL_recibido
-          print ("[", self.id, "]: actualizo TTL a ", self.TTL," \n")
-          if self.TTL > 0:
-            self.TTL -= 1
-            mensaje = ("M",[recursoBuscado,emisorBusqueda,self.TTL])
-            for vecino in self.neighbors:
-              if vecino != origen:  
-                newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
-                CaminataAleatoria.contadorMensajes += 1
+        if walkingLength == 0 || len( avalibleNodes ) == 0 :
+            return
+
+        # elige el siguiente nodo al que se le envía M
+        nextNode = random.choice( avalibleNodes ) 
+
+        # quitamos dicho nodo de la lista de disponibles
+        self.avalibleNodes.remove( nextNode )
+
+        # envía mensaje
+        newevent = Event("M", self.clock + 1.0, nextNode , self.id)
+        newevent.setIdRoot( idRoot )
+        newevent.setWalkingLength( walkingLength - 1 )
+        newevent.setResourcesToFound( resourcesToFound )
+        self.transmit(newevent)
+
+    def iHaveContent( self , resourcesToFound ) :
+        for myResource in self.myResources :
+            for requiredResource in resourcesToFound :
+                if myResources == requiredResource :
+                    return True
+
+        return False
+        
+    def iniciaHandler( self , event ) :
+        for i in range( event.getWalkers ) :
+            self.randomSend( self.avalibleNodes , event.getIdRoot() , event.getWalkingLength() , event.getResourcesToFound() )
+
+    def mHandler( self , event ) :
+        if self.visited == False :
+            self.visited = True
+            self.father = event.getSource()
+            self.avalibleNodes.remove( self.father )
+
+            if self.iHaveContent( event.getResourcesToFound() ) :
+                newevent = Event("REGRESA", self.clock + 1.0, event.getIdRoot() , self.id)
                 self.transmit(newevent)
-    elif nombre == "ENCONTRADO":
-      CaminataAleatoria.encontrado = True
-      self.nodos_con_recurso.append(origen)
-      print ("[", self.id,"]: ¡El nodo ", origen," tiene el recurso!  t = ",self.clock,"\n>>> ACTUALIZO LISTA DE NODOS CON RECURSO: ",self.nodos_con_recurso,"\n")
+
+            # envíal siguiente si es posible
+            self.randomSend( self.avalibleNodes , event.getIdRoot() , event.getWalkingLength() , event.getResourcesToFound() )
+
+        else :
+            newevent = Event("RECHAZA", self.clock + 1.0, event.getSource() , self.id)
+            self.transmit(newevent)
+
+    # Aqui se definen las acciones concretas que deben ejecutarse cuando se
+    # recibe un evento
+    def receive(self, event):
+     if nombre == "INICIA":
+         self.iniciaHandler( event )
+     elif nombre == "M":
+        self.mHandler( event )
+     elif nombre == "REGRESA":
+     
+     elif nombre == "RECHAZA":
+        CaminataAleatoria.encontrado = True
+        self.nodos_con_recurso.append(origen)
+        print ("[", self.id,"]: ¡El nodo ", origen," tiene el recurso!  t = ",self.clock,"\n>>> ACTUALIZO LISTA DE NODOS CON RECURSO: ",self.nodos_con_recurso,"\n")
   
-   def llenaRecurso(self):
-     listaRecursos =[]
-     tam = random.randint(2,10) 
-     for _ in range (tam): 
-       contenido = random.randint(1,100)
-       if contenido not in listaRecursos: 
-        listaRecursos.append(contenido)
-     return listaRecursos
+    def llenaRecurso(self):
+      listaRecursos =[]
+      tam = random.randint(2,10) 
+      for _ in range (tam): 
+         contenido = random.randint(1,100)
+         if contenido not in listaRecursos: 
+          listaRecursos.append(contenido)
+      return listaRecursos
